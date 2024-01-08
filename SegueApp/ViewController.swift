e84ff02983c7d3e203393d738b4b360f99e4fa1d
Conor Butcher <conorbutcher22@outlook.com>
2024-01-08 11:45:02 +0000

    Uncommited

--- SegueApp/ViewController.swift
+++ SegueApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var userName = ""
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
 
+    @IBAction func nextButton(_ sender: Any) {
+        userName = nameText.text!
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC" {
+            let destinationVC = segue.destination as! ViewController2
+            destinationVC.myName = userName
+        }
+    }
+    
 
 }
 
--- SegueApp/ViewController2.swift
+++ SegueApp/ViewController2.swift
@@ -8,22 +8,16 @@
 import UIKit
 
 class ViewController2: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var myName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameLabel.text = myName
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
